f43f9ee0e0f69466b23f8cd9ceddd4cf08069fe7
tim <tjc@gfxmonk.net>
2009-05-13 08:58:57 +1000

    tests...

--- test/watcher_test.py
+++ test/watcher_test.py
@@ -1,18 +1,52 @@
 import test_helper
 
 from mocktest import *
+from sniffles.watcher import *
+from sniffles import watcher as watcher_module
 
 #TODO: fill in all these tests!
 
 class WatcherTest(TestCase):
 	def test_should_be_enabled_when_given___sniffles_option(self):
-		pass
+		watcher = Watcher()
+		self.assertFalse(watcher.enabled)
+		config = mock('config').with_children(sniffles=True)
+		watcher.configure(config.raw)
+		mock_on(watcher)._setup
+		self.assertTrue(watcher.enabled)
 	
 	def test_should_be_enabled_when_NOSE_SNIFFLES_is_set(self):
-		pass
-	
+		watcher = Watcher()
+		self.assertFalse(watcher.enabled)
+		environ = {'NOSE_SNIFFLES':True)
+		parser = mock('parser')
+		parser.expects('add_option').with(
+			'--sniffles',
+			action='store_true',
+			default=True,
+			dest='sniffles',
+			help=anything)
+		watcher.options(parser.raw, environ)
+
+	def test_should_not_be_enabled_when_NOSE_SNIFFLES_is_set(self):
+		watcher = Watcher()
+		self.assertFalse(watcher.enabled)
+		environ = {'NOSE_SNIFFLES':False)
+		parser = mock('parser')
+		parser.expects('add_option').with(
+			'--sniffles',
+			action='store_true',
+			default=False,
+			dest='sniffles',
+			help=anything)
+		watcher.options(parser.raw, environ)
+
 	def test_should_scan_for_state_if_not_given_any(self):
-		pass
+		watcher = Watcher()
+		state = mock('state')
+		
+		mock_on(watcher_module).scanner.expects('scan').once().returning(mock_state.raw)
+		#TODO
 	
 	def test_should_only_run_affected_and_bad_files(self):
 		pass
