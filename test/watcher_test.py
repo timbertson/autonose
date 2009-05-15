3293441c8a44f6a1818e12f68e3a06ee2a360852
tim <tjc@gfxmonk.net>
2009-05-15 19:40:04 +1000

    more untested tests

--- test/watcher_test.py
+++ test/watcher_test.py
@@ -44,24 +44,57 @@ class WatcherTest(TestCase):
 	def test_should_scan_for_state_if_not_given_any(self):
 		watcher = Watcher()
 		state = mock('state')
-		
+		config = mock('config').with_children(sniffles=True)
 		mock_on(watcher_module).scanner.expects('scan').once().returning(mock_state.raw)
-		#TODO
+		watcher.configure(config.raw)
 	
 	def test_should_only_run_affected_and_bad_files(self):
-		pass
+		watcher = Watcher()
+		good = set([1,2,3])
+		bad = set([4,5,6])
+		changed = seet([7,8,9])
+		state = mock('state').with_children(good=good, bad=bad, changed=changed)
+		config = mock('config').with_children(sniffles=True)
+		mock_on(watcher_module).scanner.expects('scan').once().returning(mock_state.raw)
+		watcher.configure(config.raw)
+		
+		self.assertEqual(watcher.files_to_run, bad.union(good))
 
 	def test_should_update_test_when_run(self):
-		pass
+		#TODO: deal with formatError & split these tests into 4
+		good = mock('good')
+		bad = mock('bad')
+		ugly = mock('ugly')
+		skippy = mock('skippy')
+		watcher = Watcher()
+
+		update_test = mock_on(watcher)._update_test
+		update_test.is_expected.exactly(4).times
+		
+		update_test.is_expected.once()with_(good.raw, watcher_module.success)
+		update_test.is_expected.once()with_(bad.raw, watcher_module.failure)
+		update_test.is_expected.once()with_(ugly.raw, watcher_module.error)
+		update_test.is_expected.once()with_(ugly.raw, watcher_module.skippy)
+		
+		watcher.beforeTest(good)
+		watcher.addSuccess(good)
+	
+		watcher.beforeTest(bad)
+		watcher.addFailure(bad)
+	
+		watcher.beforeTest(ugly)
+		watcher.addError(ugly)
+
+		watcher.beforeTest(skippy)
+		watcher.afterTest(skippy)
 	
 	def test_should_update_test_forr_each_result_type(self):
 		pass
 
-	def test_should_ensure_every_test_had_a_result(self):
-		pass
-	
 	def test_should_save_state_on_finalize(self):
-		pass
+		state.expects('save').once()
+		watcher.finalize()
+		#??
 
 
 
