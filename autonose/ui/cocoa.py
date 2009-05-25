7c4e436e9ae41dbf4af056bb653fde9fdaf20c80
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-05-25 20:32:01 +1000

    more cocoa

--- autonose/ui/cocoa.py
+++ autonose/ui/cocoa.py
@@ -1,7 +1,20 @@
 import os
+import threading
+from Cocoa import *
+
 
 class Cocoa(object):
 	def __init__(self, nose_args):
+		self.setup_args(nose_args)
+		print dir(Cocoa)
+		self.app = NSApplication.sharedApplication()
+		threading.Thread(target=self._main).start()
+	
+	def _main(self):
+		# NSBundle.loadNibNamed_owner_("main", NSApp)
+		self.app.run()
+	
+	def setup_args(self, nose_args):
 		nose_args += ['--xml', '--xml-formatter=nosexml.PrettyPrintFormatter']
 		try:
 			del os.environ['NOSE_REDNOSE']
@@ -9,6 +22,7 @@ class Cocoa(object):
 
 	def finalize(self):
 		pass
+
 	def begin_new_run(self, current_time):
 		pass
 
