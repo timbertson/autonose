4ae96b806d5dface49809560e68d432b55d3ed3e
Tim Cuthbertson <tim3d.junk@gmail.com>
2009-06-21 00:56:47 +1000

    test helper adds mocktest to the import path if it's not already there

--- test/test_helper.py
+++ test/test_helper.py
@@ -4,3 +4,6 @@ import os
 
 logging.getLogger('test').setLevel(logging.DEBUG)
 autonose_root = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+if not autonose_root in sys.path:
+	sys.path.insert(0, autonose_root)
+
