4b03a379282208b8e2b099e95dc425f47ef41e46
gfxmonk <tim3d.junk@gmail.com>
2011-01-04 13:56:24 +1100

    remove OSX UI, as it is unmaintained and no longer works

--- autonose/ui/platform.py
+++ autonose/ui/platform.py
@@ -2,11 +2,9 @@ import sys
 
 def default_app():
 	platform = sys.platform
-	app = None
+	App = None
 	if platform.startswith('linux'):
 		from gtkapp import App
-	elif platform == 'darwin':
-		from cocoa import App
 	else:
 		raise RuntimeError("Unknown platform name: %r" % (platform,))
 	return App
